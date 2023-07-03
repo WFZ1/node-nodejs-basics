48e679569df44873fdbe0158567b711fd577ab26
WFZ1 <pavelskumenko@gmail.com>
2023-07-03 11:12:30 +0300

    refactor: file system create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,20 +1,13 @@
 import { writeFile } from 'node:fs/promises';
 import path from 'node:path';
 import { fileURLToPath } from 'url';
-import { checkFileExists } from './utils/checkFileExists.js';
 import { throwError } from './utils/throwError.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const src = path.resolve(__dirname, 'files', 'fresh.txt');
 
 const create = async () => {
-    const isFileExists = await checkFileExists(src);
-
-    if (isFileExists) {
-        throwError();
-    }
-
-    await writeFile(src, 'I am fresh and young');
+    await writeFile(src, 'I am fresh and young', { flag: 'wx' }).catch(() => throwError());
 };
 
 await create();
