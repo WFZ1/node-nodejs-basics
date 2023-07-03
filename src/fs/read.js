a5f826f2e40bfbd4aa8ede93eccfade1160049e4
WFZ1 <pavelskumenko@gmail.com>
2023-07-03 22:30:19 +0300

    feat: implement printing file content

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import { readFile } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { throwError } from './utils/throwError.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const src = path.resolve(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const content = await readFile(src, { encoding: 'utf8' }).catch(() => throwError());
+    console.log(content);
 };
 
 await read();
