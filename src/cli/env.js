a97b8a8a7b524e7fad0ec3fd11f3b866dd76a954
WFZ1 <pavelskumenko@gmail.com>
2023-07-04 12:41:11 +0300

    feat: implement parsing env variables with prefix 'RSS_'

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    const rssEnvVars = Object.entries(process.env)
+        .filter(([key]) => key.startsWith('RSS_'))
+        .map((envVar) => envVar.join('='));
+
+    if (rssEnvVars.length) {
+        const rssEnvVarsString = rssEnvVars.join('; ');
+        console.log(rssEnvVarsString);
+    }
 };
 
 parseEnv();
