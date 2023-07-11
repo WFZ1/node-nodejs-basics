994bae8b250b6fe2a4f34e924a323eb3b20a7490
WFZ1 <pavelskumenko@gmail.com>
2023-07-11 23:14:38 +0300

    feat: implement writing data to file using Writable Stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import { createWriteStream } from 'node:fs';
+import { getFilePath } from '../utils/getFilePath.js';
+
+const dest = getFilePath(import.meta.url, ['files', 'fileToWrite.txt']);
+
 const write = async () => {
-    // Write your code here 
+    const writableStream = createWriteStream(dest);
+    process.stdin.pipe(writableStream);
 };
 
 await write();
